1e67514fe22b0fd2f2ba3c00dd180e91f5844534
Kazuyuki TAKASE <tkskzyk0925@gmail.com>
2022-06-08 09:47:23 +0900

    初期配置をランダムに決めるアルゴリズム (暫定版) を実装

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const piece01 = document.querySelector('.pos-1');
+for (let n = 1; n <= 16; n = n + 1) {
+  const piece = document.querySelector('.pos-' + n);
 
-piece01.style.order = parseInt(Math.random() * 16) + 1;
+  // MEMO: 1 ～ 16 でランダムに数値を決めると、番号が被る場合がある
+  piece.style.order = parseInt(Math.random() * 16) + 1;
+}
